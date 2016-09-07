418b4f51118a9ef720db86741db9ae39ba9a5a13
isaac <imulaudzi@gmail.com>
2016-09-07 21:36:17 +0200

    helloW

---
+++ ConversationPlatformPersistence/src/main/java/com/isaac/conversationplatformpersistence/model/Like.java
@@ -0,0 +1,35 @@
+package com.isaac.conversationplatformpersistence.model;
+
+/**
+ * Created by isaac on 2016/09/07.
+ */
+public class Like {
+
+    private UserDetails userId;
+    private Topic topicId;
+    private Comment commentId;
+
+    public UserDetails getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UserDetails userId) {
+        this.userId = userId;
+    }
+
+    public Topic getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(Topic topicId) {
+        this.topicId = topicId;
+    }
+
+    public Comment getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Comment commentId) {
+        this.commentId = commentId;
+    }
+}
